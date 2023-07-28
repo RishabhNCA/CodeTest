a8f56f0e63da46a2c5f071b0aba892135dc2df16
Rishabh <rishabh300799@gmail.com>
2023-07-28 14:52:16 +0530

    handle exception

--- design/HomePage.js
+++ design/HomePage.js
@@ -3,26 +3,30 @@ const test_data_url = "../json/code-test.json";
 async function getData(url) {
     const response = await fetch(url);
     var data = await response.json();
-    if (response) {
+    if (response.status === 200) {
       show(data);
-    }
+    } else {
+      return Promise.reject(response);
+  }
 }
 
 getData(test_data_url);
 
+function redirect(link) {
+  window.location = link;
+}
+
 function show(data) {
-  document.getElementById('categoryBox').innerHTML = data?.articles?.map((articles, index) => {
-   return (`<a href=${articles?.link} class='category-link' id=${index}>
-              <div class='box-container' key=${index}>
+  document.getElementById('category-box').innerHTML = data?.articles?.map((articles, index) => {
+   return (`<div class='box-container' key=${index}>
                 <div class='left-container'>
-                  <div class='primary-name'>${articles?.primarySectionRouteName}</div>
-                  <div class='headline'>${articles?.headline}</div>
-                  <div class='standfirst'>${articles?.standfirst}</div>
+                  <div class='primary-name' onclick=(redirect('${articles?.link}'))>${articles?.primarySectionRouteName}</div>
+                  <div class='headline' onclick=(redirect('${articles?.link}'))>${articles?.headline}</div>
+                  <div class='standfirst' onclick=(redirect('${articles?.link}'))>${articles?.standfirst}</div>
                 </div>
                 <div class='right-container'>
-                  <img class='thumbnail' src=${articles?.thumbnail?.src} alt=${articles?.thumbnail?.title} />
+                  <img class='thumbnail' src=${articles?.thumbnail?.src} alt=${articles?.thumbnail?.title} onclick=(redirect('${articles?.link}')) />
                 </div>
               </div>
-            </a>
             <hr>`
 )}).join('')};
